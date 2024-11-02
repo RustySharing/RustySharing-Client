1bd54e6eddfd430118881ca309e1c35274ec24c0
omarelfouly <omarelfouly@aucegypt.edu>
2024-11-02 18:20:33 +0200

    working tcp

--- src/client.rs
+++ src/client.rs
@@ -1,22 +1,30 @@
-use rpc_client::image_encode_service::{ connect, image_encode };
 use rpc_client::gui_driver::start_gui;
+use rpc_client::image_encode_service::{connect, image_encode};
 use std::fs::File;
 use std::fs::*;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = connect()
+        .await
+        .max_decoding_message_size(100 * 1024 * 1024) // Set to 10 MB
+        .max_decoding_message_size(100 * 1024 * 1024);
 
-  let mut client = connect().await;
+    // let mut image_file = File::open("/home/ahmedwaseemr@auc.egy/Downloads/trump7.jpeg").unwrap();
+    //let mut image_data = Vec::new();
+    //image_data = image_file.bytes().map(|byte| byte.unwrap()).collect();
 
- // let mut image_file = File::open("/home/ahmedwaseemr@auc.egy/Downloads/trump7.jpeg").unwrap();
-  //let mut image_data = Vec::new();
-  //image_data = image_file.bytes().map(|byte| byte.unwrap()).collect();
+    let response = image_encode(
+        &mut client,
+        "/home/omarelfouly@auc.egy/RustySharing-Client/input_image.png",
+        2000,
+        3000,
+    )
+    .await;
 
-  let response = image_encode(&mut client, "/home/ahmedwaseemr@auc.egy/Downloads/kamala.jpeg", 2000, 3000).await;
+    println!("RESPONSE={:?}", response);
 
-  println!("RESPONSE={:?}", response);
+    // Start the GUI
+    start_gui();
 
-  // Start the GUI
-  start_gui();
-
-  Ok(())
+    Ok(())
 }
--- src/image_encode_service.rs
+++ src/image_encode_service.rs
@@ -1,37 +1,55 @@
+use image::{ImageBuffer, Rgba};
 use image_encoding::image_encoder_client::ImageEncoderClient;
-use image_encoding::{ EncodedImageRequest };
+use image_encoding::EncodedImageRequest;
 use std::fs::File;
-use std::io::{ Read, Write };
+use std::io::Read;
 
 pub mod image_encoding {
-  tonic::include_proto!("image_encoding");
+    tonic::include_proto!("image_encoding");
 }
 
 pub async fn connect() -> ImageEncoderClient<tonic::transport::Channel> {
-  ImageEncoderClient::connect("http://10.7.17.128:50051").await.unwrap()
+    ImageEncoderClient::connect("http://10.7.16.54:50051")
+        .await
+        .unwrap()
 }
 
 pub async fn image_encode(
-  client: &mut ImageEncoderClient<tonic::transport::Channel>,
-  image_path: &str,
-  width: i32,
-  height: i32
+    client: &mut ImageEncoderClient<tonic::transport::Channel>,
+    image_path: &str,
+    width: i32,
+    height: i32,
 ) -> String {
-  let mut image_file = File::open(image_path).unwrap();
-  let mut image_data = Vec::new();
-  image_data = image_file.bytes().map(|byte| byte.unwrap()).collect();
+    let image_file = File::open(image_path).unwrap();
+    let mut image_data = Vec::new();
+    image_data = image_file.bytes().map(|byte| byte.unwrap()).collect();
 
-  let request = tonic::Request::new( EncodedImageRequest {
-    width: width,
-    height: height,
-    image_data: image_data,
-  });
+    let request = tonic::Request::new(EncodedImageRequest {
+        width,
+        height,
+        image_data,
+    });
 
-  let response = client.image_encode(request).await.unwrap();
+    println!("Sending ...");
 
-  // write encoded image to path
-  let mut encoded_image_file = File::create("encoded_image.png").unwrap();
-  encoded_image_file.write_all(&response.get_ref().image_data).unwrap();
+    let response = client.image_encode(request).await.unwrap();
 
-  "Encoded image saved to encoded_image.png".to_string()
+    println!("Sent!");
+
+    let encoded_data = &response.get_ref().image_data;
+    let width = response.get_ref().width;
+    let height = response.get_ref().height;
+    let decoded_image = ImageBuffer::<Rgba<u8>, Vec<u8>>::from_raw(
+        width.try_into().unwrap(),
+        height.try_into().unwrap(),
+        encoded_data.clone(),
+    )
+    .expect("Failed to create ImageBuffer from raw data");
+    // write encoded image to path
+    let output_file_path = "encoded_image.png";
+    decoded_image
+        .save(output_file_path)
+        .expect("Failed to save encoded image");
+
+    "Encoded image saved to encoded_image.png".to_string()
 }
