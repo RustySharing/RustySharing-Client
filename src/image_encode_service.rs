bea8a3d07805f715804697ff88e81f912f7c6a29
ahmedwaseem77 <ahmedwaseemr@aucegypt.edu>
2024-11-09 18:15:51 +0200

    start signal

--- src/image_encode_service.rs
+++ src/image_encode_service.rs
@@ -7,6 +7,11 @@ use steganography::util::{ bytes_to_file, file_to_bytes };
 use crate::utils::get_file_name;
 use std::thread;
 
+// TODO: put these in a config file or environment variables
+const MAX_SERVICE_ATTEMPTS: u32 = 3;
+// maximum number of times a client can connect to a server, and on server connect, that server fails to provide leader
+//const MAX_LEADER_PROVIDER_ATTEMPTS: u32 = 3;
+
 pub mod image_encoding {
   tonic::include_proto!("image_encoding");
 }
@@ -15,38 +20,62 @@ pub mod leader_provider {
   tonic::include_proto!("leader_provider");
 }
 
-pub async fn connect() -> LeaderProviderClient<tonic::transport::Channel> {}
+async fn get_leader_provider_client(
+  server_list: Vec<&str>
+) -> Option<LeaderProviderClient<tonic::transport::Channel>> {
+  for _ in 0..MAX_SERVICE_ATTEMPTS {
+    for server in &server_list {
+      let socket = format!("http://{}:50051", server);
+
+      match LeaderProviderClient::connect(socket.clone()).await {
+        Ok(client) => {
+          println!("Connected to a provider at: {}", socket.clone());
+          Some(client);
+        }
+        Err(e) => {
+          println!("Failed to connect to a provider server at: {}: {}", socket, e);
+        }
+      }
+    }
+
+    println!("All servers failed waiting for 50ms then retrying");
+    thread::sleep(std::time::Duration::from_millis(50));
+  }
+
+  None
+}
+
+// async fn persist_leader_provider_client(
+//   leader_provider_client: LeaderProviderClient<tonic::transport::Channel>
+// ) -> LeaderProviderClient<tonic::transport::Channel> {
+//   let request = tonic::Request::new(leader_provider::LeaderRequest {});
+//   let response = leader_provider_client.get_leader(request).await.unwrap();
+// }
 
 pub async fn connect() -> ImageEncoderClient<tonic::transport::Channel> {
   let args: Vec<String> = std::env::args().collect();
 
   let do_random_selection = args.iter().any(|arg| arg == "--random-selection");
-  // random server selection
-  // TODO: replaced with querying service directory
+
+  // TODO: server_list replaced with querying service directory
   let server_list: Vec<&str> = vec!["10.7.16.11", "10.7.17.128", "10.7.16.54"];
 
+  let mut rng = rand::thread_rng();
+  let random_number = rng.gen_range(0..server_list.len()); // Correcting to use the length of the list
+  let random_socket = format!("http://{}:50051", server_list[random_number]);
+
   if do_random_selection {
-    // Initialize random number generator
-    let mut rng = rand::thread_rng();
-    let random_number = rng.gen_range(0..server_list.len()); // Correcting to use the length of the list
-
-    // Format the connection string with the chosen server
-    let address = format!("http://{}:50051", server_list[random_number]);
-
     // Attempt to connect to the server
-    ImageEncoderClient::connect(address).await.unwrap();
+    return ImageEncoderClient::connect(random_socket).await.unwrap();
   }
 
-  // connect to all servers on different threads
-  for server in server_list {
-    thread::spawn(|| {
-      let address = format!("http://{}:50051", server);
-      LeaderProviderClient::connect(address).await.unwrap();
-    });
-  }
-
-  // multicast to server list, 50052 will be port of leader election
-  let address = format!("http://{}:50052".server_list[random_number]);
+  let mut leader_provider_client = get_leader_provider_client(server_list).await.unwrap_or_else(|| {
+    panic!("Failed to connect to any leader provider server");
+  });
+  let request = tonic::Request::new(leader_provider::LeaderProviderEmptyRequest {});
+  let response = leader_provider_client.get_leader(request).await.unwrap();
+  let leader_socket = format!("http://{}:50051", response.get_ref().leader_socket);
+  ImageEncoderClient::connect(leader_socket).await.unwrap()
 }
 
 use crate::image_decode::decode_image;
